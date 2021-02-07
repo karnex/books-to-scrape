fa44c731f320987fa8b2d5f29a439213fe070285
karnex <mazoyer.bruno@yahoo.fr>
2021-02-07 13:58:05 +0100

    refactorize the three modules into only one including the three functions, as well as code improvement

--- run_scraper.py
+++ run_scraper.py
@@ -1,9 +1,9 @@
 import os
 import pathlib
-
 import pandas as pd
 
-from scraper import extract_category_url_from_homepage, extract_data_from_product, extract_product_url_from_category
+from scraper.collect_data import collect_category_url_from_homepage, collect_product_url_from_category, \
+    collect_data_from_product
 
 # Project directories
 PROJECT_DIRECTORY = pathlib.Path().absolute()
@@ -12,17 +12,17 @@ PRODUCTS_DIRECTORY = os.path.join(PROJECT_DIRECTORY, 'products')
 if __name__ == '__main__':
 
     # Get every categories as a dict
-    category_dict = extract_category_url_from_homepage.home_page('http://books.toscrape.com/')
+    category_dict = collect_category_url_from_homepage('http://books.toscrape.com/')
 
     for key, value in category_dict.items():  # Loop on the categories dict
 
         # Get every product URLs as a list
-        product_list = extract_product_url_from_category.category_page(value)
+        product_list = collect_product_url_from_category(value)
 
         for product_page in product_list:  # Loop on each product from a category
 
             # Get every data from a product page
-            data_dict = extract_data_from_product.product_page(product_page)
+            data_dict = collect_data_from_product(product_page)
 
             # Declare the current csv file
             filename = key.replace(' ', '').replace('\n', '') + '.csv'
--- scraper/collect_data.py
+++ scraper/collect_data.py
@@ -1,7 +1,6 @@
 import os
 import pathlib
 import urllib
-
 import requests
 
 from bs4 import BeautifulSoup
@@ -12,14 +11,90 @@ PRODUCTS_DIRECTORY = os.path.join(PROJECT_DIRECTORY, 'products')
 THUMBNAILS_DIRECTORY = os.path.join(PRODUCTS_DIRECTORY, 'thumbnails')
 
 
-def product_page(product_page):
+def collect_category_url_from_homepage(home_page):
     """
-    From the product page, extract and return some useful data as a dict
+    From the home page, collect and return some useful data as a dict
+    """
+    response = requests.get(home_page)
+
+    # When a valid response is returned
+    if response.ok:
+
+        # Format the response to encoding utf-8
+        response.encoding = 'utf-8'
+
+        # Parse the returned content and initialize the dictionary to return
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # Extract useful data and add them into the dictionary
+        category_tags = soup.find('ul', {'class': 'nav-list'}).find('ul').findAll('a')
+
+        dict_to_return = {}
+
+        for category_tag in category_tags:
+
+            # dict_to_return.update({category_tag.text: home_page + category_tag['href']})
+            dict_to_return[category_tag.text] = home_page + category_tag['href']
+
+        # Finally, return the dictionary
+        return dict_to_return
+
+    # Otherwise when an error is occurs, return an exception with the status code
+    else:
+
+        raise Exception(response.status_code)
+
+
+def collect_product_url_from_category(category_page):
+    """
+    From the category page, colelct and return every product URL as a list
+    """
+    list_to_return = []  # List that will be returned
+    i = 1  # var to increment
+
+    while True:
+
+        if i > 1:
+            response = requests.get(category_page.replace('index.html', f'page-{i}.html'))
+        else:
+            response = requests.get(category_page)
+
+        # When a valid response is returned
+        if response.ok:
+
+            # Format the response to encoding utf-8
+            response.encoding = 'utf-8'
+
+            # Parse the returned content and initialize the list to return
+            soup = BeautifulSoup(response.text, 'html.parser')
+
+            product_tags = soup.find('section').findAll('div', {'class': 'image_container'})
+
+            for product_tag in product_tags:
+                list_to_return.append(category_page + '/../' + product_tag.find('a')['href'])
+
+            i += 1
+
+            # Otherwise when an error is occurs
+        else:
+
+            break  # Leave the loop
+
+    # Finally, return the list
+    return list_to_return
+
+
+def collect_data_from_product(product_page):
+    """
+    From the product page, collect and return some useful data as a dict
     """
     response = requests.get(product_page)
 
     # When a valid response is returned
     if response.ok:
+
+        # Format the response to encoding utf-8
+        response.encoding = 'utf-8'
 
         # Parse the returned content and initialize the dictionary to return
         soup = BeautifulSoup(response.text, 'html.parser')
@@ -32,10 +107,10 @@ def product_page(product_page):
         title = soup.find('div', {'class': 'product_main'}).find('h1').text
         dict_to_return.update({'title': title})
 
-        price_incl_tax = soup.find('table', {'class': 'table-striped'}).findAll('td')[3].text.replace('Â', '')
+        price_incl_tax = soup.find('table', {'class': 'table-striped'}).findAll('td')[3].text
         dict_to_return.update({'price_including_tax': price_incl_tax})
 
-        price_excl_tax = soup.find('table', {'class': 'table-striped'}).findAll('td')[2].text.replace('Â', '')
+        price_excl_tax = soup.find('table', {'class': 'table-striped'}).findAll('td')[2].text
         dict_to_return.update({'price_excluding_tax': price_excl_tax})
 
         availability = soup.find('table', {'class': 'table-striped'}).findAll('td')[5].text
